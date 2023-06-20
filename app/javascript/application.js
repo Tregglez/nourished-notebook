8ed08e36b8a2e3a450741d067a18b61dfffa3b4c
Benjamin Treglown <ben@Benjamins-MacBook-Air.local>
2023-06-20 10:57:11 +1000

    added textbox for paragraph writing for the author view. Can click text to edit and save to hide.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,31 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+import "trix";
+import "@rails/actiontext";
+import Sortable from 'sortablejs';
+
+
+document.addEventListener('turbo:load', () => {
+
+  document.addEventListener('click', () => {
+    let element = event.target.closest('.paragraph-content');
+    if (!element) return;
+
+    element.classList.add('d-none');
+    element.nextElementSibling.classList.remove('d-none');
+  });
+
+  document.addEventListener('click', () => {
+    if (!event.target.matches('.cancel')) return;
+
+    event.preventDefault();
+
+    let element = event.target.closest('.paragraph-form');
+
+    element.classList.add('d-none');
+    element.previousElementSibling.classList.remove('d-none');
+  });
+  let sortable_elements = document.getElementById('elements');
+  Sortable.create(sortable_elements, { animation: 150 });
+});
